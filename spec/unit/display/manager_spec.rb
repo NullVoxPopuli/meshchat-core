4d7c8ce3df1a1a004f95f64fb2ad7534382aee30
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-10-21 14:20:08 -0400

    add display specs

---
+++ spec/unit/display/manager_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe MeshChat::Display::Manager do
+  let (:klass){ MeshChat::Display::Manager }
+
+  before(:each) do
+    allow(MeshChat::Net::Client).to receive(:send){}
+
+    mock_settings_objects
+  end
+
+  describe '#present_message' do
+
+    it 'invokes chat' do
+      expect(MeshChat::Instance.display).to receive(:chat)
+      m = MeshChat::Message::Chat.new
+      MeshChat::Display.present_message(m)
+    end
+
+    it 'invokes whisper' do
+      expect(MeshChat::Instance.display).to receive(:whisper)
+      m = MeshChat::Message::Whisper.new
+      MeshChat::Display.present_message(m)
+    end
+
+    it 'invokes info' do
+      expect(MeshChat::Instance.display).to receive(:info)
+      m = MeshChat::Message::PingReply.new
+      MeshChat::Display.present_message(m)
+    end
+
+    it 'invokes add_line for other menssages' do
+      expect(MeshChat::Instance.display).to receive(:add_line)
+      m = MeshChat::Message::Disconnection.new
+      MeshChat::Display.present_message(m)
+    end
+  end
+
+end
